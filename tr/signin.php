16bd4e872b51f6de30537397a382bad228dee8f1
Neer-7303 <53005830+Neer-7303@users.noreply.github.com>
2020-11-26 21:17:45 +0530

    Update signin.php

--- tr/signin.php
+++ tr/signin.php
@@ -19,7 +19,7 @@ function check_input($value)
 return $value;
 }
 
-$con = mysqli_connect("localhost", "root", "");
+$con = mysqli_connect("localhost", "root", "",'technotip');
 if (!$con)
   {
   die('Could not connect: ' . mysqli_error());
